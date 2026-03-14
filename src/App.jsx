0fd3a7d203856112488027d56345606a8cd55687
AdSoyad <email@example.com>
2026-03-14 15:43:36 +0400

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,55 @@ const App = () => {
       title: "Harvest Vase",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
       price: 78,
-      imageUrl: "https://images.unsplash.com/photo-1687106358396-20daadef9e36?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c21hbGwlMjBwbGFudHxlbnwwfHwwfHx8MA%3D%3D"
+      imageUrl: "https://hips.hearstapps.com/hmg-prod/images/palm-tree-in-pink-pot-royalty-free-image-1727982899.jpg?crop=0.88933xw:1xh;center,top"
     },
     {
       title: "Green Plant",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
       price: 45,
-      imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3JlZW4lMjBwbGFudHxlbnwwfHwwfHx8MA%3D%3D"
+      imageUrl: "https://www.bhg.com/thmb/LzF_YQm9oucr8El0XVmJIpGcRg0=/1335x0/filters:no_upscale():strip_icc()/plumeria-flowers-0144ca65-2000-e8a8b629cf00427893d57cc411cd3f27.jpg"
     },
     {
       title: "Succulent Plant",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
       price: 30,
-      imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3VjY3VsZW50JTIwcGxhbnR8ZW58MHx8MHx8MA%3D%3D"
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/62/Money_Plant_%28Epipremnum_aureum%29_4.jpg"
     },
     {
       title: "Flower Pot",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
       price: 60,
-      imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2VyJTIwcG90fGVufDB8fDB8fA%3D%3D"
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/2/2b/Money_Plant_%28Epipremnum_aureum%29_3.jpg"
     },
     {
       title: "Cactus Plant",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
       price: 25,
-      imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FjdHVzfGVufDB8fDB8fA%3D%3D"
+      imageUrl: "https://www.joyusgarden.com/wp-content/uploads/2018/01/rubber.jpg"
     },
     {
       title: "Orchid Plant",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
       price: 90,
-      imageUrl: "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b3JjaGlkfGVufDB8fDB8fA%3D%3D"
+      imageUrl: "https://www.bhg.com/thmb/7T-D-VRiKQ84nhfHYqxlnZLdooM=/1080x0/filters:no_upscale():strip_icc()/heuchera-midnight-bayou-2af3c0c3-d7f31fd6147449d384e2f35f6ee72718.jpg"
+    },
+    {
+      title: "Tree",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
+      price: 90,
+      imageUrl: "https://www.gardendesign.com/pictures/images/675x529Max/site_3/beautifall-summer-nights-pothos-pothos-plant-proven-winners_18890.jpg"
+    },
+    {
+      title: "Jasmin",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
+      price: 90,
+      imageUrl: "https://img.freepik.com/free-photo/red-flower-tilt-shift-lens_417767-654.jpg"
+    },
+    {
+      title: "Flower",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veritatis aspernatur ex? Sit ducimus, ad amet repellendus delectus quidem qui.",
+      price: 90,
+      imageUrl: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1191062920.jpg?crop=1.00xw:0.834xh;0,0.0420xh"
     }
   ]
 
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -12,27 +12,29 @@ const Cart = ({ plant }) => {
         setCount(prevState => prevState !== 1 ? prevState -1 : 1)
     }
   return (
-    <div className='flex max-w-125 h-fit rounded-xl bg-white overflow-hidden shadow-lg shadow-green-500'>
-      <img className='w-50' src={plant.imageUrl} alt="" />
-    <div className='px-5 py-10'>
+<div className='flex flex-col md:flex-row max-w-125 w-full h-fit rounded-xl bg-white overflow-hidden shadow-lg shadow-green-500'>
+  <img className='w-full md:w-50 h-64 md:h-auto object-cover' src={plant.imageUrl} alt="" />
+  <div className='px-5 py-10'>
 
-      <h2 className='font-semibold text-2xl'>{plant.title}</h2>
-      <p className='text-sm text-zinc-400'>BY STUDIO AND FRIENDS</p>
+    <h2 className='font-semibold text-xl sm:text-2xl'>{plant.title}</h2>
+    <p className='text-xs sm:text-sm text-zinc-400'>BY STUDIO AND FRIENDS</p>
 
-      <p className='my-5 text-zinc-500'>{plant.description}</p>
+    <p className='my-5 text-sm sm:text-base text-zinc-500'>{plant.description}</p>
 
-      <div className='flex gap-5 items-center'>
-        
-        <div className="flex gap-3 font-semibold text-2xl border">
-            <button className="size-8 bg-red-600 text-white flex justify-center items-center hover:cursor-pointer" onClick={decrement}><Minus/></button>
-            <p>{count}</p>
-            <button className="size-8 bg-green-600 text-white flex justify-center items-center hover:cursor-pointer" onClick={increment}><Plus/></button>
-        </div>
-
-        <button className='bg-green-800 text-white px-4 py-2 rounded-4xl font-bold cursor-pointer shadow-2xl shadow-green-500'>BUY NOW ({plant.price * count}$)</button>
+    <div className='flex flex-col sm:flex-row gap-5 items-center'>
+      
+      <div className="flex gap-3 font-semibold text-xl sm:text-2xl border">
+        <button className="size-8 bg-red-600 text-white flex justify-center items-center hover:cursor-pointer" onClick={decrement}><Minus/></button>
+        <p>{count}</p>
+        <button className="size-8 bg-green-600 text-white flex justify-center items-center hover:cursor-pointer" onClick={increment}><Plus/></button>
       </div>
+
+      <button className='bg-green-800 text-white px-4 py-2 rounded-4xl font-bold cursor-pointer shadow-2xl shadow-green-500 w-full sm:w-auto text-sm sm:text-base'>
+        BUY NOW ({plant.price * count}$)
+      </button>
     </div>
-    </div>
+  </div>
+</div>
   )
 }
 
